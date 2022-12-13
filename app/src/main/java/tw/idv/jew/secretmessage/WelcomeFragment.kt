5be0812541f1d1fc70f6892d807f707b2596270a
a0916105 <whdofjew@gmail.com>
2022-12-13 20:28:08 +0800

    findNavController()

--- app/src/main/java/tw/idv/jew/secretmessage/WelcomeFragment.kt
+++ app/src/main/java/tw/idv/jew/secretmessage/WelcomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
 
 class WelcomeFragment : Fragment() {
 
@@ -13,6 +15,14 @@ class WelcomeFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         // 將這個fragment的layout充氣
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+        val view = inflater.inflate(R.layout.fragment_welcome, container, false)
+
+        val startButton = view.findViewById<Button>(R.id.start)
+
+        startButton.setOnClickListener {
+            view.findNavController()    //取得導覽控制器
+                .navigate(R.id.action_welcomeFragment_to_messageFragment)   //巡覽至MessageFragment（使用action ID）
+        }
+        return view
     }
 }
